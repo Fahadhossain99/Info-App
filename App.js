8ac81005d44a253430fa7d48b1b5392c60f8d7f1
Fahad Hossain <64582073+Fahadhossain99@users.noreply.github.com>
2022-02-28 14:50:04 +0600

    material things create

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import { typography } from './src/theme';
 
 export default function App() {
+   const [loaded] = useFonts({
+    'Antonio-Medium':require('./assets/fonts/Antonio-Medium.ttf'),
+    'Spartan-Bold':require('./assets/fonts/Spartan-Bold.ttf'),
+    'Spartan-Regular':require('./assets/fonts/Spartan-Regular.ttf'),
+   });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily:typography.bold}} >Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
