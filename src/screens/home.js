9678c5e0b8f3fbbb3e88ea4437320d24591db587
Fahad Hossain <64582073+Fahadhossain99@users.noreply.github.com>
2022-03-01 13:19:52 +0600

    Data input

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,10 +1,43 @@
-import { Pressable, View } from 'react-native'
+import { Pressable, StatusBar, View ,FlatList} from 'react-native'
 import React from 'react'
 import Text from '../components/text/text'
 import { colors } from './../theme/colors';
 import InfoHeader from '../components/Info-header';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+
+export const STUDENT_LIST = [
+  {
+    name: "Fahad",
+    description:
+      "I am Fahad. I am from Dhaka.I am a student of Dhaka International University.",
+    image: require("../../assets/Fahad.jpg"),
+  },
+  {
+    name: "Sanjida",
+    description:
+      "I am Sanjida. I am from Dhaka.  I am a student of Eden Collage.",
+    image: require("../../assets/Sanjida.jpg"),
+  },
+  {
+    name: "Marzan",
+    description:
+      "I am marzan. I am from Bogura.I am a student of Dhaka International University.",
+    image: require("../../assets/Marzan .jpg"),
+  },
+  {
+    name: "Saadman",
+    description:
+      "I am Saadman. I am from Spain.I am a student of Dhaka International University.",
+    image: require("../../assets/Saadman.jpg"),
+  },
+  {
+    name: "Bapp",
+    description:
+      "I am marzan. I am from Dhaka.I am a student of Dhaka International University.",
+    image: require("../../assets/Bappy .jpg"),
+  },
+];
 
 export default function Home({navigation}) {
   return (
@@ -13,6 +46,7 @@ export default function Home({navigation}) {
      <Pressable onPress={() => navigation.navigate("Details")}>
 <Text> Details page </Text>
      </Pressable>
+     <StatusBar backgroundColor={colors.black} barStyle='light-content'/>
     </SafeAreaView>
   );
 }
