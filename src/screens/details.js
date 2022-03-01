70850c2edc03086375764e72f9265ffc27fe655d
Fahad Hossain <64582073+Fahadhossain99@users.noreply.github.com>
2022-03-01 14:20:32 +0600

    update details

--- src/screens/details.js
+++ src/screens/details.js
@@ -1,14 +1,52 @@
-import { View } from 'react-native'
+import { View,SafeAreaView, ScrollView,StyleSheet, Image, Dimensions, } from 'react-native'
 import React from 'react'
 import Text from '../components/text/text'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import InfoHeader from '../components/Info-header'
-import { colors } from './../theme/colors';
 
-export default function Details() {
+import InfoHeader from './../components/Info-header';
+
+
+
+
+export default function Details({route}) {
+    const { topic } = route.params;
+    const { name, description, image } = topic;
   return (
-    <SafeAreaView style={{backgroundColor:colors.offwhite,flex:1}}>
-      <InfoHeader/>
+    <SafeAreaView>
+      <InfoHeader />
+      <ScrollView>
+        <View>
+          {image ? (
+            <Image
+              source={image}
+              style={{
+                alignSelf: "center",
+                width: Dimensions.get("screen").width,
+                height: 200,
+                paddingTop: 300,
+              }}
+              resizeMode="contain"
+            />
+          ) : null}
+          <Text style={styles.textStyleName}>{name}</Text>
+          <Text style={styles.textStyleDetails}>{description}</Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
+const styles = StyleSheet.create({
+  textStyleName: {
+    fontSize: 45,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+    marginLeft: 150,
+    marginTop: 20,
+    marginBottom: 20,
+    color: "#006400",
+  },
+  textStyleDetails: {
+    fontSize: 25,
+    margin: 10,
+    color: "#800000",
+  },
+});
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,9 +1,18 @@
-import { Pressable, StatusBar, View ,FlatList} from 'react-native'
+import {
+  Pressable,
+  StatusBar,
+  View,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import React from 'react'
 import Text from '../components/text/text'
 import { colors } from './../theme/colors';
 import InfoHeader from '../components/Info-header';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { spacing } from './../theme/spacing';
+// import { TouchableOpacity } from "react-native-web";
 
 
 export const STUDENT_LIST = [
@@ -32,21 +41,56 @@ export const STUDENT_LIST = [
     image: require("../../assets/Saadman.jpg"),
   },
   {
-    name: "Bapp",
+    name: "Bappy",
     description:
       "I am marzan. I am from Dhaka.I am a student of Dhaka International University.",
     image: require("../../assets/Bappy .jpg"),
   },
 ];
 
+const styles = StyleSheet.create({
+  item: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: spacing[12],
+    borderBottomColor: colors.black,
+    justifyContent: "space-between",
+  },
+  txtStyle: {
+    fontSize: 26,
+    fontStyle: "italic",
+  },
+  viewStyle: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 8,
+    borderRadius: 25,
+    margin: 10,
+    backgroundColor: "#FFFDD0",
+  },
+});
 export default function Home({navigation}) {
+  const renderItem = ({item,index}) =>{
+     const { name } = item;
+    return (
+      <TouchableOpacity onPress={() => navigation.navigate("Details",{topic:item})} style={styles.viewStyle}>
+        <View>
+          <Text style={styles.txtStyle}> {name} </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
   return (
     <SafeAreaView style={{ backgroundColor: colors.offwhite, flex: 1 }}>
-     <InfoHeader/>
-     <Pressable onPress={() => navigation.navigate("Details")}>
-<Text> Details page </Text>
-     </Pressable>
-     <StatusBar backgroundColor={colors.black} barStyle='light-content'/>
+      <InfoHeader />
+      <FlatList 
+      data={STUDENT_LIST}
+      renderItem={renderItem}
+      keyExtractor={(item,index)=>item.name}
+      contentContainerStyle={{margin:spacing[5]}}
+
+      />
+      <StatusBar backgroundColor={colors.black} barStyle="light-content" />
     </SafeAreaView>
   );
 }
